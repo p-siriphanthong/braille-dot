65d932faf703f4508d61875e8fe0d9ee5f18a943
p-siriphanthong <p.siriphanthong@gmail.com>
2018-11-12 02:30:20 +0700

    Setup production deployment to Heroku

--- server/index.js
+++ server/index.js
@@ -85,10 +85,12 @@ app.get('/api/braille/word/random', (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  // Serve any static files
+  app.use(express.static(path.join('client/build')))
 
+  // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    res.sendFile(path.join('client/build', 'index.html'))
   })
 }
 
